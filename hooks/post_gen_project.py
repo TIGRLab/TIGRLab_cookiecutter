47b0d739cdae06d61a82c1438bfea328f7a7d440
Gabi Herman <gabiherman@gmail.com>
2020-03-06 13:53:41 -0500

    made a post gen congrats msg and prompt to pls look at the readme

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,7 @@
+repo_name = '{{ cookiecutter.project_folder_name }}'
+
+print ("\n************************************************************************************")
+print ("\nCongrats! Your data analysis project directory has been created at: %s" %repo_name)
+
+print ("\n************************************************************************************")
+print("\nPlease refer to README.md for instructions on creating a github repo to track your analyses")
