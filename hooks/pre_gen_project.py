dd5b231603f8f4243b988bbc14545ecf8a22b0c5
Gabi Herman <gabiherman@gmail.com>
2020-03-06 11:20:54 -0500

    added option about whether to set up a github repo

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -10,3 +10,11 @@ if not re.match(MODULE_REGEX, module_name):
     print('ERROR: %s is not a valid name for a folder or git repo! No spaces or special characters allowed.' % module_name)
     # exits with status 1 to indicate failure
     sys.exit(1)
+
+
+module_name2 = '{{ cookiecutter.set_up_git_repo }}'
+
+if not (re.match(r'^y', module_name2, re.IGNORECASE)|re.match(r'^n', module_name2, re.IGNORECASE)):
+    print('ERROR: Must answer yes or no (y or n) for whether to make a github repo %s.' % module_name2)
+    # exits with status 1 to indicate failure
+    sys.exit(1)
